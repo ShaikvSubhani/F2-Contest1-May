847b644ddbacf410b9e1df04ce6ce126862c1144
shaik subhani <118998017+ShaikvSubhani@users.noreply.github.com>
2023-05-14 09:34:54 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", marks: 80 },
+  { id: 2, name: "jack", age: "20", marks: 85 },
+  { id: 3, name: "karen", age: "19", marks: 35 },
+];
+
+function PrintStudentswithMap() {
+  //Write your code here , just console.log
+}
+
+function PrintStudentsbyForEach() {
+  //Write your code here , just console.log
+}
+
+function addData() {
+  //Write your code here, just console.log
+}
+
+function removeFailedStudent() {
+  //Write your code here, just console.log
+}
+
+function concatenateArray() {
+  //Write your code here, just console.log
+}
